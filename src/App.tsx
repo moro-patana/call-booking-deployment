39fed67955ebb53a81ca10f142446184f1a667dd
Developer <daniel.her@onja.org>
2022-11-03 14:07:58 +0300

    [Feat]-Display days of the week
    [Refactor]-Pass selected date as props

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
+import TableContainer from "@mui/material/TableContainer";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import DaysOfWeek from "./components/daysOfWeek";
 
 function App() {
+  const selectedDate = new Date();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <TableContainer>
+        <Table>
+          <TableBody>
+            <DaysOfWeek {...{ selectedDate }} />
+          </TableBody>
+        </Table>
+      </TableContainer>
     </div>
   );
 }
