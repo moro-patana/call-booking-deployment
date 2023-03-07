6c27859091d7b87c75b603aa1d670ceb90375bfb
Prisca  Roseline <60210128+ravinalamada@users.noreply.github.com>
2023-03-07 10:14:04 +0300

    Moving BookingModal components' styles to its own CSS module file (#70)
    Use CSS Module for BookingModal component

--- src/components/bookingModal/BookingModal.tsx
+++ src/components/bookingModal/BookingModal.tsx
@@ -1,219 +1,181 @@
 import React, { FC, useCallback, useState } from "react";
 import { Box, Button, Modal, SelectChangeEvent, TextField, Typography } from "@mui/material";
 import AccessTimeIcon from "@mui/icons-material/AccessTime";
-import styled from "styled-components";
-
 import { fetchBookingsByUser } from "../../redux/actions/bookings";
 import { useAppDispatch, useAppSelector } from "../../redux/hooks";
-
 import SelectInput from "../Select/SelectInput";
 import DatePicker from "../datePicker/DatePicker";
-
 import { RoomType } from "../../utils/types";
 import { getSelectedTimeMinutes, newDateGenerator, timeConverter } from "../../utils/dateUtils";
 import { bookingMutation, sendAuthorizedQuery } from '../../graphqlHelper';
+import styles from './bookingModal.module.css';
 
 interface BookingModalProps {
-    rooms: RoomType[];
-    repeatData: { name: string; id: string }[];
-    open: boolean;
-    handleClose: () => void;
-    position: { x: number; y: number };
-    day: Date;
-    date: Date;
-    startDate: Date;
-    endDate: Date;
-    selectedRoom: string;
-    setSelectedRoom: (value: string) => void;
+  rooms: RoomType[];
+  repeatData: { name: string; id: string }[];
+  open: boolean;
+  handleClose: () => void;
+  position: { x: number; y: number };
+  day: Date;
+  date: Date;
+  startDate: Date;
+  endDate: Date;
+  selectedRoom: string;
+  setSelectedRoom: (value: string) => void;
 }
 
 interface NewBooking {
-    roomId: string;
-    label: string;
-    startDate: any;
-    endDate: any;
-    token: string;
+  roomId: string;
+  label: string;
+  startDate: any;
+  endDate: any;
+  token: string;
 }
 
 const BookingModal: FC<BookingModalProps> = ({
-    rooms,
-    repeatData,
-    open,
-    handleClose,
-    position,
-    date,
-    startDate,
-    endDate,
-    selectedRoom,
-    setSelectedRoom
+  rooms,
+  repeatData,
+  open,
+  handleClose,
+  position,
+  date,
+  startDate,
+  endDate,
+  selectedRoom,
+  setSelectedRoom
 }) => {
-    const { currentUser } = useAppSelector(state => state.users);
-    const dispatch = useAppDispatch();
-    const start = new Date(startDate);
-    const end = new Date(endDate);
-    const selectedHour = timeConverter(start?.getHours());
-    const [label, setLabel] = useState("");
-    const [roomId, setRoomId] = useState(selectedRoom);
-    const [repeatEvent, setRepeatEvent] = useState(repeatData[0].id);
-    const [startTime, setStartTime] = useState(
-        `${selectedHour}:${getSelectedTimeMinutes(date, 0)}`
+  const { currentUser } = useAppSelector(state => state.users);
+  const dispatch = useAppDispatch();
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+  const selectedHour = timeConverter(start?.getHours());
+  const [label, setLabel] = useState("");
+  const [roomId, setRoomId] = useState(selectedRoom);
+  const [repeatEvent, setRepeatEvent] = useState(repeatData[0].id);
+  const [startTime, setStartTime] = useState(
+    `${selectedHour}:${getSelectedTimeMinutes(date, 0)}`
+  );
+  const [endTime, setEndTime] = useState(
+    `${selectedHour}:${getSelectedTimeMinutes(date, date?.getMinutes() + 15)}`
+  );
+  const { modal, box, typography, backdrop, datePickerWrapper, buttonWrapper, textField } = styles;
+
+  const boxPosition = {
+    left: position.x > 70 ? "70%" : `${position.x}%`,
+    top: position.y > 518 ? 518 : position.y > 18 ? position.y : 18,
+  };
+
+  const handleStartTimeEvent = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setStartTime(event.target.value);
+  };
+
+  const handleEndTimeEvent = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setEndTime(event.target.value);
+  };
+
+  const handleChange = (value: Date | null) => {
+    return value;
+  };
+
+  const handleRepeatEventChange = (event: SelectChangeEvent<any>) => {
+    setRepeatEvent(event.target.value);
+  };
+
+  const addNewBooking = async (userBooking: NewBooking) => {
+    const { roomId, label, startDate, endDate, token } = userBooking;
+    const response = await sendAuthorizedQuery(
+      bookingMutation(roomId, label, startDate, endDate),
+      token
     );
-    const [endTime, setEndTime] = useState(
-        `${selectedHour}:${getSelectedTimeMinutes(date, date?.getMinutes() + 15)}`
-    );
+    const booking = response.data.data;
+    return booking;
+  };
 
-    const handleStartTimeEvent = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setStartTime(event.target.value);
-    };
+  const handleSubmitBooking = useCallback(async () => {
+    const newStartDate = newDateGenerator(start, startTime);
+    const newEndDate = newDateGenerator(end, endTime);
 
-    const handleEndTimeEvent = (event: React.ChangeEvent<HTMLInputElement>) => {
-        setEndTime(event.target.value);
-    };
+    if (currentUser.login.token && roomId) {
+      await addNewBooking({
+        label,
+        startDate: newStartDate,
+        endDate: newEndDate,
+        roomId,
+        token: currentUser.login.token,
+      });
 
-    const handleChange = (value: Date | null) => {
-        return value;
-    };
-
-    const handleRepeatEventChange = (event: SelectChangeEvent<any>) => {
-        setRepeatEvent(event.target.value);
-    };
-
-    const addNewBooking = async (userBooking: NewBooking) => {
-        const { roomId, label, startDate, endDate, token } = userBooking;
-        const response = await sendAuthorizedQuery(
-            bookingMutation(roomId, label, startDate, endDate),
-            token
-        );
-        const booking = response.data.data;
-        return booking;
-    };
-
-    const handleSubmitBooking = useCallback(async () => {
-        const newStartDate = newDateGenerator(start, startTime);
-        const newEndDate = newDateGenerator(end, endTime);
-
-        if (currentUser.login.token && roomId) {
-            await addNewBooking({
-                label,
-                startDate: newStartDate,
-                endDate: newEndDate,
-                roomId,
-                token: currentUser.login.token,
-            });
-
-            dispatch(fetchBookingsByUser());
-        }
-
-        handleClose();
-        // eslint-disable-next-line react-hooks/exhaustive-deps
-    }, [label, roomId, startTime, endTime]);
-
-    return (
-        <div>
-            <Modal
-                open={open}
-                onClose={handleClose}
-                sx={{
-                    display: "flex",
-                    flexDirection: "column",
-                    "& > .MuiBackdrop-root": {
-                        backgroundColor: "transparent",
-                        cursor: "pointer",
-                    },
-                }}
-            >
-                <Box
-                    sx={{
-                        width: "100%",
-                        maxWidth: "364px",
-                        backgroundColor: "#fff",
-                        padding: "19px 24px",
-                        margin: "auto",
-                        outline: 0,
-                        boxShadow: "0px 0px 6px #c8c8c8",
-                        display: "flex",
-                        flexDirection: "column",
-                        position: "absolute",
-                        left: position.x > 70 ? "70%" : `${position.x}%`,
-                        top: position.y > 518 ? 518 : position.y > 18 ? position.y : 18,
-                    }}
-                >
-                    <Typography
-                        variant="h3"
-                        sx={{ fontSize: "14px", marginBottom: "18px" }}
-                    >
-                        Book a room
-                    </Typography>
-
-                    <TextField
-                        label="label"
-                        sx={{ marginBottom: "16px" }}
-                        onChange={(event) => setLabel(event.target.value)}
-                        InputLabelProps={{
-                            shrink: true,
-                        }}
-                    />
-                    <DatePickerWrapper
-                        style={{
-                            marginBottom: "14px",
-                            display: "flex",
-                            alignItems: "center",
-                        }}
-                    >
-                        <AccessTimeIcon />
-                        <DatePicker
-                            value={startDate}
-                            handleChange={handleChange}
-                            startTime={startTime}
-                            endTime={endTime}
-                            startTimeOnChange={handleStartTimeEvent}
-                            endTimeOnChange={handleEndTimeEvent}
-                        />
-                    </DatePickerWrapper>
-                    <SelectInput
-                        handleChange={(event: SelectChangeEvent<any>) => {
-                            setSelectedRoom(event.target.value)
-                            setRoomId(event.target.value)
-                        }}
-                        data={rooms}
-                        defaultValue={selectedRoom}
-                        value={roomId}
-                        note="There are available rooms"
-                    />
-                    <SelectInput
-                        handleChange={handleRepeatEventChange}
-                        data={repeatData}
-                        value={repeatEvent}
-                        note="Select repeat options"
-                    />
-                    <Wrapper className="button-wrapper">
-                        <Button onClick={handleClose}>Cancel</Button>
-                        <Button onClick={handleSubmitBooking}>Book</Button>
-                    </Wrapper>
-                </Box>
-            </Modal>
-        </div>
-    );
-};
-
-const DatePickerWrapper = styled("div")`
-  svg {
-    margin-right: 16px;
-    height: 20px;
-  }
-`;
-
-const Wrapper = styled("div")`
-  &.button-wrapper {
-    display: flex;
-    place-content: end;
-    margin-top: auto;
-    button {
-      padding: 9px 7px;
-      color: #6200ee;
-      font-size: 14px;
+      dispatch(fetchBookingsByUser());
     }
-  }
-`;
+
+    handleClose();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [label, roomId, startTime, endTime]);
+
+  return (
+    <div>
+      <Modal
+        open={open}
+        onClose={handleClose}
+        className={modal}
+        slotProps={{backdrop: { className: backdrop }}}
+      >
+        <Box
+          className={box}
+          sx={boxPosition}
+        >
+          <Typography
+            variant="h3"
+            className={typography}
+            id={styles.typography}
+          >
+            Book a room
+          </Typography>
+
+          <TextField
+            label="label"
+            className={textField}
+            onChange={(event) => setLabel(event.target.value)}
+            InputLabelProps={{
+              shrink: true,
+            }}
+          />
+          <Box
+            className={datePickerWrapper}
+          >
+            <AccessTimeIcon />
+            <DatePicker
+              value={startDate}
+              handleChange={handleChange}
+              startTime={startTime}
+              endTime={endTime}
+              startTimeOnChange={handleStartTimeEvent}  
+              endTimeOnChange={handleEndTimeEvent}  
+            />
+          </Box>
+          <SelectInput
+            handleChange={(event: SelectChangeEvent<any>) => {
+              setSelectedRoom(event.target.value)
+              setRoomId(event.target.value)
+            }}
+            data={rooms}
+            defaultValue={selectedRoom}
+            value={roomId}
+            note="There are available rooms"
+          />
+          <SelectInput
+            handleChange={handleRepeatEventChange}
+            data={repeatData}
+            value={repeatEvent}
+            note="Select repeat options"
+          />
+          <Box className={buttonWrapper}>
+            <Button onClick={handleClose}>Cancel</Button>
+            <Button onClick={handleSubmitBooking}>Book</Button>
+          </Box>
+        </Box>
+      </Modal>
+    </div>
+  )
+};
 
 export default BookingModal;
--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { Provider } from 'react-redux';
-import store from './redux/store';
+import { StyledEngineProvider } from '@mui/material';
 import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import reportWebVitals from './reportWebVitals';
+import store from './redux/store';
+import App from './App';
+import './index.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
+
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <Provider store={store}>
-        <App />
-      </Provider>
+      <StyledEngineProvider injectFirst>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </StyledEngineProvider>
     </BrowserRouter>
   </React.StrictMode>
 );
